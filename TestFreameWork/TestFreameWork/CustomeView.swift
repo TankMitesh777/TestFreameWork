9dc7fdc327dfd5cb2d5f2fb8c780f5d971912e31
NetDroid-Mac4 <netdroid-mac4@NetDroid-Mac4s-Mac-mini.local>
2018-09-20 11:01:29 +0530

    Finish my Work

---
+++ TestFreameWork/TestFreameWork/CustomeView.swift
@@ -0,0 +1,19 @@
+//
+//  CustomeView.swift
+//  TestFreameWork
+//
+//  Created by NetDroid-Mac4 on 20/09/18.
+//  Copyright © 2018 NetDroid-Mac4. All rights reserved.
+//
+
+import Foundation
+
+public class CustomeView {
+    
+    private init() {}
+    
+    public static func doSomething() -> String
+    {
+        return "Its Netdroid Testing Freamework"
+    }
+}
---
+++ TestFreameWork/TestFreameWork/TestFreameWork.h
@@ -0,0 +1,19 @@
+//
+//  TestFreameWork.h
+//  TestFreameWork
+//
+//  Created by NetDroid-Mac4 on 20/09/18.
+//  Copyright © 2018 NetDroid-Mac4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFreameWork.
+FOUNDATION_EXPORT double TestFreameWorkVersionNumber;
+
+//! Project version string for TestFreameWork.
+FOUNDATION_EXPORT const unsigned char TestFreameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFreameWork/PublicHeader.h>
+
+
